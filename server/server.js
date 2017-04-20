5fa6b6433d9fc323c1d9cde80a84da331a26af42
Max Berger <maxamillianberger@gmail.com>
2017-04-19 19:19:12 -0700

    Add prettier

--- server/server.js
+++ server/server.js
@@ -4,15 +4,11 @@ var path = require('path');
 var routes = require('./routes');
 var bodyParser = require('body-parser');
 var port = process.env.PORT || 3000;
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, '../client')));
 app.use(routes);
-
 app.listen(port, function() {
   console.log('listening on', port);
 });
-
-
 module.exports = app;
